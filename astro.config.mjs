c759598de116753edd103b40b9dad02bd35d8552
Angel Gala <galafloresangelemilio@gmail.com>
2025-04-03 16:40:07 -0500

    update page home

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Agencia Servitec',
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
